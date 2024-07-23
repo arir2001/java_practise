0e62ef99286df7f2db1c12534695b3cb73a92290
arir2001 R <amtrooney@gmail.com>
2024-07-23 16:20:04 +0000

    arrow function video.

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,46 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b){
+    ///code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(9,10);
+console.log(sum);
+
 
 // Arrow Function With Parameters
+//define as conostannt soo they cannot be overwritten oor re defined. 
+const addTwoNumberConst = (a, b) => {
+    ///code block
+    return a + b;
+}
+
+let sum2 = addTwoNumberConst(9,10);
+console.log(sum2);
+
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a+b;
+//if function is simple, just use fat arrow
+let sum3 = addTwoNumbers2(9,10);
+console.log(sum2);
 
 // Implicit Returns
+const saysomething = (message) => console.log(message)
+
+saysomething('Well, bitch')
+
+const sayHello = () => console.log('hello')
+sayHello()
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    '<p>
+        This is a multiline string!
+    </p>'
+)
+
+console.log(returnMultipleLines())
